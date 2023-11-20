ff11600d674064f740940c9f304bfa321b35b5de
SanderMiesen <sander.miesen@epfl.ch>
2023-11-20 12:29:49 +0100

    Added second initializer

--- flygym/mujoco/arena/__init__.py
+++ flygym/mujoco/arena/__init__.py
@@ -1,3 +1,4 @@
 from .base import BaseArena, FlatTerrain
 from .complex_terrain import GappedTerrain, BlocksTerrain, MixedTerrain
 from .sensory_environment import OdorArena
+from .food_environment import OdorArenaEnriched
